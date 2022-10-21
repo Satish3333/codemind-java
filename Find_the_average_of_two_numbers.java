fc51c112bb134b746578f01a6176001e60f7be19
Satish3333 <102217053+Satish3333@users.noreply.github.com>
2022-10-21 22:38:35 +0545

    import java.util.Scanner; class area {     public static void main(String args[])     {         Scanner sc=new Scanner (System.in);         float a,b;         a=sc.nextFloat();         b=sc.nextFloat();         System.out.printf("%.4f",(a+b)/2);     }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class area
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner (System.in);
+        float a,b;
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        System.out.printf("%.4f",(a+b)/2);
+    }
+    
+}
