4f93c210120d2bb9b22acdccf16d637aeaed59d6
Satish3333 <102217053+Satish3333@users.noreply.github.com>
2022-10-23 19:47:47 +0545

    import java.util.Scanner; class main {     public static int prime(int a){         int c=0;         for(int i=1;i<=a;i++){             if (a%i==0)             c+=1;         }         if (c==2)         return 1;         else         return 0;     }     public static void main(String args[])     {     Scanner s=new Scanner(System.in);         int n=s.nextInt();         int [] l=new int[n];         int d=0;         for(int i=0;i<n;i++){             l[i]=s.nextInt();         }         int k=s.nextInt();         for (int i=0;i<n;i++){             if (l[i]<=k && prime(l[i])==1)             d+=1;         }         System.out.println(d);     } }

---
+++ primes_less_than_k.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class main
+{
+    public static int prime(int a){
+        int c=0;
+        for(int i=1;i<=a;i++){
+            if (a%i==0)
+            c+=1;
+        }
+        if (c==2)
+        return 1;
+        else
+        return 0;
+    }
+    public static void main(String args[])
+    {
+    Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int [] l=new int[n];
+        int d=0;
+        for(int i=0;i<n;i++){
+            l[i]=s.nextInt();
+        }
+        int k=s.nextInt();
+        for (int i=0;i<n;i++){
+            if (l[i]<=k && prime(l[i])==1)
+            d+=1;
+        }
+        System.out.println(d);
+    }
+}
