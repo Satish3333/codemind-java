b46cecc7ea7fe692c241fec267d1bb98355506b1
Satish3333 <102217053+Satish3333@users.noreply.github.com>
2022-10-23 22:44:32 +0545

    import java.util.Scanner; class main {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         double n=s.nextDouble();         double t=0.0;         t=((n*9)/5)+32;         System.out.printf("%.2f",t);     } }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        double n=s.nextDouble();
+        double t=0.0;
+        t=((n*9)/5)+32;
+        System.out.printf("%.2f",t);
+    }
+}
+        
+        
+        
